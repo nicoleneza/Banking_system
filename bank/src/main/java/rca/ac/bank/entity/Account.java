531b2ad243ae470f1d4cc6aa20103aa32e931aff
nicoleneza <rwegoneza12@gmail.com>
2024-07-02 11:26:17 +0200

    first commit

---
+++ bank/src/main/java/rca/ac/bank/entity/Account.java
@@ -0,0 +1,25 @@
+package rca.ac.bank.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "owner")
+    private String accountHolder;
+    private double balance;
+}
